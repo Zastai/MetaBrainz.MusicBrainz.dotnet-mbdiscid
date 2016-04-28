a2d4cfffd25a3f2332b70cfd1f10e44875134fa8
Tim Van Holder <tim.vanholder@gmail.com>
2016-04-28 19:31:59 +0200

    First version: - top-level README and simplistic Travis build file - class library project corresponding to libmusicbrainz; currently empty - class library project corresponding to libdiscid; currently feature-complete for libdiscid 0.6.1 - sample console application for the libdiscid functionality

---
+++ DiscId/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MetaBrainz.MusicBrainz.DiscId {
+
+  internal static class Program {
+
+    private static void Main(string[] args) {
+      try {
+      var device = ((args.Length == 0) ? new CdDevice() : new CdDevice(args[0]));
+        Console.WriteLine($"Disc Device: {device.Name}");
+        Console.WriteLine();
+        device.ReadDisc();
+        var toc = device.TableOfContents;
+        if (toc == null)
+          Console.WriteLine("No table of contents available.");
+        else {
+          Console.WriteLine($"Media Catalog Number: {toc.MediaCatalogNumber}");
+          Console.WriteLine($"MusicBrainz Disc ID : {toc.DiscId}");
+          Console.WriteLine($"FreeDB Disc ID      : {toc.FreeDbId}");
+          Console.WriteLine($"Submission URL      : {toc.SubmissionUrl}");
+          Console.WriteLine();
+          Console.WriteLine("Tracks:");
+          foreach (var t in toc.Tracks)
+            Console.WriteLine($" {t.Number,2}. Duration: {t.Duration} ISRC: {t.Isrc}");
+        }
+      }
+      catch (Exception e) {
+        Console.WriteLine($"[error] {e}");
+      }
+      if (Debugger.IsAttached)
+        Console.ReadKey();
+    }
+
+  }
+
+}
---
+++ DiscId/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle        ("DiscId")]
+[assembly: AssemblyDescription  ("Small tool to get a disc's discid, MCN and ISRC")]
+[assembly: AssemblyCompany      ("MetaBrainz")]
+[assembly: AssemblyProduct      ("libdiscid")]
+[assembly: AssemblyCopyright    ("Copyright © Tim Van Holder 2016")]
+
+[assembly: ComVisible(false)]
+
+// This should be MAJOR.MINOR, matching libdiscid.
+[assembly: AssemblyVersion    ("0.6")]
+// This should be MAJOR.MINOR.PATCHLEVEL, with MAJOR.MINOR matching libdiscid and PATCHLEVEL as a release sequence number (not following libdiscid).
+[assembly: AssemblyFileVersion("0.6.0")]
