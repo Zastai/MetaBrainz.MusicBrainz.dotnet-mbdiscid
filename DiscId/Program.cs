36f697a7f50f55d27730ad0eddaa6dd73356ff9d
Tim Van Holder <tim.vanholder@gmail.com>
2016-06-21 22:45:15 +0200

    Improved the DiscId utility. Track ISRC values from CD-TEXT data are now shown too. Added -noisrc and -nomcn command line options to inhibit reading ISRC and MCN info, respectively. Don't show any output related to MCN, ISRC or CD-TEXT when the corresponding feature is not enabled (either because not supported, or inhibited via command line option).

--- DiscId/Program.cs
+++ DiscId/Program.cs
@@ -35,24 +35,29 @@ namespace MetaBrainz.MusicBrainz.DiscId {
 
     private static int Main(string[] args) {
       try {
-        var showText = true;
+        var features = TableOfContents.AvailableFeatures;
         string device = null;
         foreach (var arg in args) {
           switch (arg) {
-            case "help": case "-?": case "/?":
-              Console.WriteLine($"Supported Read Features: {string.Join(", ", TableOfContents.ReadFeatures)}");
+            case "help": case "-?": case "/?": {
+              Console.WriteLine($"Supported Read Features: {TableOfContents.AvailableFeatures}");
               Console.WriteLine();
               Console.WriteLine($"Default Device: {TableOfContents.DefaultDevice ?? "<none available>"}");
+              Console.WriteLine();
+              Console.WriteLine("Available Options:");
+              Console.WriteLine("  -noisrc       Disable reading of track ISRC values.");
+              Console.WriteLine("  -nomcn        Disable reading of the media catalog number.");
+              Console.WriteLine("  -notext       Disable reading of CD-TEXT info.");
               Console.WriteLine();
               Console.WriteLine("Available Devices:");
               var n = 0;
               foreach (var dev in TableOfContents.AvailableDevices)
                 Console.WriteLine($"{++n,3}. {dev}");
               return 0;
-            case "-notext":
-            case "/notext":
-              showText = false;
-              break;
+            }
+            case "-noisrc": case "/noisrc": features &= ~DiscReadFeature.TrackIsrc;          break;
+            case "-nomcn":  case "/nomcn":  features &= ~DiscReadFeature.MediaCatalogNumber; break;
+            case "-notext": case "/notext": features &= ~DiscReadFeature.CdText;             break;
             default:
               if (device != null)
                 throw new ArgumentException("Too many command line arguments given.");
@@ -60,19 +65,20 @@ namespace MetaBrainz.MusicBrainz.DiscId {
               break;
           }
         }
-        var toc = TableOfContents.ReadDisc(device);
+        var toc = TableOfContents.ReadDisc(device, features);
         if (toc == null)
           Console.WriteLine("No table of contents available.");
         else {
           Console.WriteLine($"CD Device Used      : {toc.DeviceName}");
           Console.WriteLine();
-          Console.WriteLine($"Media Catalog Number: {toc.MediaCatalogNumber}");
+          if ((features & DiscReadFeature.MediaCatalogNumber) != 0)
+            Console.WriteLine($"Media Catalog Number: {toc.MediaCatalogNumber ?? "* not set *"}");
           Console.WriteLine($"MusicBrainz Disc ID : {toc.DiscId}");
           Console.WriteLine($"FreeDB Disc ID      : {toc.FreeDbId}");
           Console.WriteLine($"Submission URL      : {toc.SubmissionUrl}");
           Console.WriteLine();
           var languages = toc.TextLanguages;
-          if (showText && languages?.Count > 0) {
+          if (languages?.Count > 0) {
             var text = toc.TextInfo;
             if (text?.Count > 0) {
               Console.WriteLine("CD-TEXT Information:");
@@ -87,13 +93,13 @@ namespace MetaBrainz.MusicBrainz.DiscId {
                     Console.WriteLine($"  - Genre           : {ti.Genre.Value}");
                 }
                 if (ti.Identification != null) Console.WriteLine($"  - Identification  : {ti.Identification}");
-                if (ti.ProductCode != null) Console.WriteLine($"  - UPC/EAN         : {ti.ProductCode}");
-                if (ti.Title != null) Console.WriteLine($"  - Title           : {ti.Title}");
-                if (ti.Performer != null) Console.WriteLine($"  - Performer       : {ti.Performer}");
-                if (ti.Lyricist != null) Console.WriteLine($"  - Lyricist        : {ti.Lyricist}");
-                if (ti.Composer != null) Console.WriteLine($"  - Composer        : {ti.Composer}");
-                if (ti.Arranger != null) Console.WriteLine($"  - Arranger        : {ti.Arranger}");
-                if (ti.Message != null) Console.WriteLine($"  - Message         : {ti.Message}");
+                if (ti.ProductCode    != null) Console.WriteLine($"  - UPC/EAN         : {ti.ProductCode}");
+                if (ti.Title          != null) Console.WriteLine($"  - Title           : {ti.Title}");
+                if (ti.Performer      != null) Console.WriteLine($"  - Performer       : {ti.Performer}");
+                if (ti.Lyricist       != null) Console.WriteLine($"  - Lyricist        : {ti.Lyricist}");
+                if (ti.Composer       != null) Console.WriteLine($"  - Composer        : {ti.Composer}");
+                if (ti.Arranger       != null) Console.WriteLine($"  - Arranger        : {ti.Arranger}");
+                if (ti.Message        != null) Console.WriteLine($"  - Message         : {ti.Message}");
               }
               Console.WriteLine();
             }
@@ -105,8 +111,11 @@ namespace MetaBrainz.MusicBrainz.DiscId {
               Console.WriteLine($" --- Offset: {150,6} ({Program.TwoSeconds,-16}) Length: {t.Offset - 150,6} ({t.StartTime.Subtract(Program.TwoSeconds),-16})");
           }
           foreach (var t in toc.Tracks) {
-            Console.WriteLine($" {t.Number,2}. Offset: {t.Offset,6} ({t.StartTime,-16}) Length: {t.Length,6} ({t.Duration,-16}) ISRC: {t.Isrc}");
-            if (showText && languages?.Count > 0) {
+            Console.Write($" {t.Number,2}. Offset: {t.Offset,6} ({t.StartTime,-16}) Length: {t.Length,6} ({t.Duration,-16})");
+            if ((features & DiscReadFeature.TrackIsrc) != 0)
+              Console.Write($" ISRC: {t.Isrc ?? "* not set *"}");
+            Console.WriteLine();
+            if (languages?.Count > 0) {
               var text = t.TextInfo;
               if (text?.Count > 0) {
                 Console.WriteLine("     CD-TEXT Information:");
@@ -120,6 +129,7 @@ namespace MetaBrainz.MusicBrainz.DiscId {
                   if (ti.Composer       != null) Console.WriteLine($"       - Composer  : {ti.Composer}");
                   if (ti.Arranger       != null) Console.WriteLine($"       - Arranger  : {ti.Arranger}");
                   if (ti.Message        != null) Console.WriteLine($"       - Message   : {ti.Message}");
+                  if (ti.Isrc           != null) Console.WriteLine($"       - ISRC      : {ti.Isrc}");
                 }
               }
             }
