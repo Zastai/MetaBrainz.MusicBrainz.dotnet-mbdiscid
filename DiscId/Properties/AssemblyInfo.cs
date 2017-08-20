a909f0a0b703b9316fba0c4dd337a08a345e1e7b
Tim Van Holder <tim.vanholder@gmail.com>
2017-08-20 14:12:55 +0200

    Dropped net20 and net35 as targets; added netstandard2.0. Dropped some now-unnecessary compatibility code. Use some C# 7 language features. For the DiscId tool, moved most assembly attributes into the .csproj.

--- DiscId/Properties/AssemblyInfo.cs
+++ DiscId/Properties/AssemblyInfo.cs
@@ -1,16 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle      ("DiscId")]
-[assembly: AssemblyDescription("Small tool to get a disc's discid, MCN, ISRC and/or CD-TEXT")]
-[assembly: AssemblyCompany    ("MetaBrainz")]
-[assembly: AssemblyProduct    ("libdiscid")]
-[assembly: AssemblyCopyright  ("Copyright © Tim Van Holder 2016")]
-
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion    ("0.2")]
-[assembly: AssemblyFileVersion("0.2.0")]
-
-// This should match AssemblyFileVersion, plus optional extra tags (such as a pre-release indicator).
-[assembly: AssemblyInformationalVersion("0.2.0 (pre-release)")]
+[assembly: CLSCompliant(true)]
